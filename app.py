a321215475d41cf0e0c684b0d4a35d2efb1ea2b6
kyunghoonkook <rnrrudgg68@gmail.com>
2022-09-22 10:20:14 +0900

    최종 추가 파일 및 변경 파일

--- app.py
+++ app.py
@@ -8,52 +8,51 @@ ca = certifi.where();
 client = MongoClient('mongodb+srv://test:sparta@cluster0.khegl5v.mongodb.net/Cluster0?retryWrites=true&w=majority',
                      tlsCAFile=ca)
 db = client.dbsparta
+import datetime
+from datetime import datetime, timedelta
 
-from datetime import datetime
-
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
 SECRET_KEY = 'SPARTA'
 
 import jwt
 
-import datetime
+
 
 import hashlib
 
 
 @app.route('/')
 def home():
-    token_receive = request.cookies.get('mytoken')
 
-    users = list(db.user.find({}, {"_id": False}))
-    if token_receive is None:
-        return render_template('index.html', borders=users)
+    return render_template('index.html')
 
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.user.find_one({"id": payload['id']})
-        return render_template('index.html', borders=users, nickname=user_info["nick"], id=user_info["id"])
-    except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
-    except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+    # try:
+    #     payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    #     user_info = db.user.find_one({"id": payload['id']})
+    #     return render_template('index.html', borders=users, nickname=user_info["nick"], id=user_info["id"])
+    # except jwt.ExpiredSignatureError:
+    #     return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    # except jwt.exceptions.DecodeError:
+    #     return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 
-@app.route('/membership')
-def membership():
-    return render_template('membership.html')
-
-
-@app.route('/api/membership', methods=['POST'])
-def api_membership():
-    id_receive = request.form['id_give']
-    pw_receive = request.form['pw_give']
-    nickname_receive = request.form['nickname_give']
-
-    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
-
-    db.user.insert_one({'id': id_receive, 'pw': pw_hash, 'nick': nickname_receive})
-
-    return jsonify({'result': 'success'})
+# @app.route('/membership')
+# def membership():
+#     return render_template('membership.html')
+#
+#
+# @app.route('/api/membership', methods=['POST'])
+# def api_membership():
+#     id_receive = request.form['id_give']
+#     pw_receive = request.form['pw_give']
+#     nickname_receive = request.form['nickname_give']
+#
+#     pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+#
+#     db.user.insert_one({'id': id_receive, 'pw': pw_hash, 'nick': nickname_receive})
+#
+#     return jsonify({'result': 'success'})
 
 
 @app.route('/login')
@@ -61,49 +60,90 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/api/login', methods=['POST'])
-def api_login():
-    id_receive = request.form['id_give']
-    pw_receive = request.form['pw_give']
+# @app.route('/api/login', methods=['POST'])
+# def api_login():
+#     id_receive = request.form['id_give']
+#     pw_receive = request.form['pw_give']
+#
+#     # 암호화
+#     pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+#
+#     # 유저찾기
+#     result = db.user.find_one({'id': id_receive, 'pw': pw_hash})
+#
+#     # JWT 토큰발급
+#     if result is not None:
+#
+#         payload = {
+#             'id': id_receive,
+#             'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60 * 60 * 24)
+#         }
+#         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf8')
+#
+#         # token주기
+#         return jsonify({'result': 'success', 'token': token})
+#     # 없을 시
+#     else:
+#         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+#
+#
+# @app.route('/api/nick', methods=['GET'])
+# def api_valid():
+#     token_receive = request.cookies.get('mytoken')
+#
+#     try:
+#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256']).decode('utf8')
+#         print(payload)
+#
+#         userinfo = db.user.find_one({'id': payload['id']}, {'_id': 0})
+#         return jsonify({'result': 'success', 'nickname': userinfo['nick']})
+#     except jwt.ExpiredSignatureError:
+#         return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
+#     except jwt.exceptions.DecodeError:
+#         return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
 
-    # 암호화
-    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
 
-    # 유저찾기
-    result = db.user.find_one({'id': id_receive, 'pw': pw_hash})
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
 
-    # JWT 토큰발급
+
+@app.route('/api/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
     if result is not None:
-
         payload = {
-            'id': id_receive,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60 * 60 * 24)
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf8')
 
-        # token주기
         return jsonify({'result': 'success', 'token': token})
-    # 없을 시
+    # 찾지 못하면
     else:
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,                               # 아이디
+        "password": password_hash,                                  # 비밀번호
+        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
 
-@app.route('/api/nick', methods=['GET'])
-def api_valid():
-    token_receive = request.cookies.get('mytoken')
-
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256']).decode('utf8')
-        print(payload)
-
-        userinfo = db.user.find_one({'id': payload['id']}, {'_id': 0})
-        return jsonify({'result': 'success', 'nickname': userinfo['nick']})
-    except jwt.ExpiredSignatureError:
-        return jsonify({'result': 'fail', 'msg': '로그인 시간이 만료되었습니다.'})
-    except jwt.exceptions.DecodeError:
-        return jsonify({'result': 'fail', 'msg': '로그인 정보가 존재하지 않습니다.'})
-
-
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
 @app.route('/post_list')
 def post_list():
     return render_template('post_list.html')
@@ -149,7 +189,7 @@ def show_list():
     return jsonify({'all_lists': lists})
 
 
-@app.route('/list', methods=['POST'])
+@app.route('/list/save', methods=['POST'])
 def save_list():
     title_receive = request.form['title_give']
     text_receive = request.form['text_give']
@@ -158,7 +198,7 @@ def save_list():
 
     extension = file.filename.split('.')[-1]
 
-    today = datetime.datetime.now()
+    today = datetime.now()
     mytime = today.strftime('%Y-%m-%d-%H-%M-%S')
 
     filename = f'file-{mytime}'
